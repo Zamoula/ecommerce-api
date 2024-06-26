9dd183cf41f856634ba3edabafd667c8be5170d3
Zamoula <jamelmrad000@gmail.com>
2024-06-27 00:44:02 +0100

    1.1

--- src/models/product.js
+++ src/models/product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    description: String,
+    description: { type: String, required: false },
     stock: { type: Number, required: true }
 });
 
