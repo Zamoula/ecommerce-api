be0bcb8dea2cc1c974cd92b0430f406191515d42
Zamoula <jamelmrad000@gmail.com>
2024-06-26 20:34:08 +0100

    cors added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const productRoutes = require('./src/routes/products');
 const customerRoutes = require('./src/routes/customers');
@@ -9,7 +10,9 @@ const orderRoutes = require('./src/routes/orders');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
+
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://jamelmrad000:cntrFgl0SJdJ9fOs@cluster0.s88rsdr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
